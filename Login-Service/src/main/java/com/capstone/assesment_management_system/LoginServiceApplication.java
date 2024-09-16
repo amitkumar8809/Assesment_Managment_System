f9c1753fb0d72dd004764322d45c9559ce999822
Amit kumar <amitkumarnic00hmt@gmail.com>
2024-09-16 18:25:18 +0530

    remove token details

---
+++ Login-Service/src/main/java/com/capstone/assesment_management_system/LoginServiceApplication.java
@@ -0,0 +1,15 @@
+package com.capstone.assesment_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class LoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginServiceApplication.class, args);
+	}
+
+}
---
+++ Login-Service/src/test/java/com/capstone/assesment_management_system/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.assesment_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
